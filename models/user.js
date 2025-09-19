5232e9c66262d909bac481eeafb0908e78951ac0
Siddhant <siddhantvgorte@gmail.com>
2025-09-19 19:53:28 +0530

    Initial setup

---
+++ models/user.js
@@ -0,0 +1,68 @@
+const { log } = require( 'console' )
+const { createHmac, randomBytes} = require('crypto')
+const {Schema, model} = require('mongoose')
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    salt: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profileImageURL: {
+        type: String,
+        default: '/images/default.jpeg'
+    },
+    role: {
+        type: String,
+        enum: ['USER', 'ADMIN'],
+        default: 'USER',
+    }
+}, 
+{timestamps: true})
+
+userSchema.pre('save', function (next) {
+    const user = this
+
+    if(!user.isModified('password')) return
+
+    const salt = randomBytes(16).toString()
+    const hashedPassword = createHmac('sha256', salt)
+        .update(user.password)
+        .digest('hex')
+
+        this.salt = salt
+        this.password = hashedPassword
+
+        next()
+})
+
+userSchema.static('matchPassword', async function(email, password) {
+    const user = await this.findOne({ email})
+    if(!user) throw new Error('User not found')
+    console.log(user)
+    const salt = user.salt
+    const hashedPassword = user.password
+
+    const userProvidedHash = createHmac('sha256', salt)
+        .update(password)
+        .digest('hex')
+
+    if (hashedPassword !== userProvidedHash) throw new Error('Incorrect password')
+
+    return user
+})
+
+const User = model('user', userSchema)
+
+module.exports = User
